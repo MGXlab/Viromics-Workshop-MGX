3b8e19390b6eb390b094c10c9f18fbca5cbb7434
jeroenmeijer <meijeroen@gmail.com>
2022-02-10 22:10:46 +0100

    now clusters bins

--- _episodes/42_gene_content.md
+++ _episodes/42_gene_content.md
@@ -31,13 +31,13 @@ $ rstudio &
 ~~~
 {: .bash}
 
-Open the `day4_clustering.R` script and follow the steps below.
+Open the `code/day4/clustering.R` script and follow the steps below.
 
 #### Step 1. Data exploration
-First, read the data from `annotated_clusters.txt` in R and take a moment to look at it. What information is contained in the different columns?
+First, load the libraries and read the data from `annotated_clusters.txt` in R. Take a moment to look at what was in that file. What's in each of the different columns?
 
 #### Step 2. Cluster contigs based on gene content
-Follow the steps in the script to explore and clean up the data (lines 10-34 of the script). Finally, cluster the assembled contigs based on protein clusters (PCs).   
+Follow the steps in the script to explore and clean up the data (up to line 40). Finally, cluster the assembled contigs based on protein clusters (PCs).   
 
 >## Question: Clustering contigs based on shared protein clusters
 > Try a few different length cutoffs. Explain what you see
@@ -46,48 +46,43 @@ Follow the steps in the script to explore and clean up the data (lines 10-34 of 
 
 **Q: explain what you see?**  
 A:
-- Sparse clustering indicates high viral diversity.
-- Small clusters of sequences that share genes.
+- Sparse clustering indicates high viral diversity. Contigs share only a very limited set of proteins.
+- There are small clusters of sequences that share genes. Are these viral families? Genera? Let's try to find out!
 
 To annotate our contigs, we need to **add sequences with known taxonomy**.
-
-
-*Question: Why not add ALL RefSeq viral genomes to heatmap?*
 
 #### Step 3. Add RefSeq Genomes ####
 Now add the selected reference sequences with known taxonomy to your heatmap. Can we infer the taxonomy for some of our contigs?
 
-
 (Note: here we need to add the reference set, which is still to be determined by Dani.
-Previously I made a selection of RefSeq sequences by finding for each contig the closest RefSeq sequence (euclidean distance based on shared proteins, from the total set of protein clusters that are included in the heatmap -- so not total nr of proteins in a contig). But we want to use the same set of RefSeq sequences for this heatmap and the tree.
+Previously I made a selection of RefSeq sequences by finding for each contig the closest RefSeq sequence (euclidean distance based on shared proteins, from the total set of protein clusters that are included in the heatmap. I can include that as a back-up plan, but we decided to use the same set of RefSeq sequences for the heatmap and the gene tree.
 
 Observations:
-- most sequences do not cluster with a RefSeq sequence. So most viral diversity is uncharted and cannot be annotated in this way.
-
+- most sequences do not cluster with a RefSeq sequence. So most viral diversity is uncharted and cannot be annotated in this way: there are no closely related sequences in RefSeq, or perhaps our clusterin method isn't sensitive enough.
+**Q:** can you think of a way to improve this?  
 
 #### Step 4. Add gene annotations ####
-Although there are no marker genes that are shared across all viruses or even bacteriophages, for some taxonomic groups there are (sets) of marker genes that can be used to reconstruct the phylogeny. For caudovirales there are three such genes: portal, major capsid protein, and the terminase large subunit.
+Although there are no marker genes that are shared across all viruses or even bacteriophages (like 16S for prokaryotes), for some taxonomic groups there are (sets) of marker genes that can be used to reconstruct the phylogeny. For *caudovirales* there are three such genes: portal, major capsid protein, and the terminase large subunit.
 
 **Challenge:**
-Find out of the terminase large subunit proteins are all in a single PC, or if they are split accross many different PCs, for example by examining the clusters or annotating the heatmap columns. How does this influence the gene sharing approach we use here?
+Find out of the terminase large subunit proteins are all in a single PC, or if they are split across many different PCs, for example by examining the clusters or annotating the heatmap columns (take a look at the [ComplexHeatmap documentation](https://jokergoo.github.io/ComplexHeatmap-reference/book/) to see how to do this). How does this influence the gene sharing approach we use here?
 
 
 **TO DO**
 - add conda environment (Jeroen)
+- add solution code to annotation challenge
 - add binning information (Jeroen)
 
 ## Dropped because we switched to using bins instead of contigs:
-
-**What potential technical problems do you see with this approach?**
-
+**Q:** What potential technical problems do you see with this approach?
 - contigs could be **genome fragments** rather than complete genomes, which would limit clustering (not true if using bins)  
 - protein clustering could not be sensitive enough, e.g. homologous proteins (such as terminase large subunit) could be split across different PCs, which would display as a lack of clustering.
 
-Discussion point:
-How does the diversity within protein families (clusters) relate to viral taxonomy (and the sparseness of sampling of viral diversity).
+**Discussion point:**
+How does the diversity within protein families (clusters) relate to viral taxonomy (and the sparseness of sampling of viral diversity)?
 
 
-#### Challenge: add CheckV completeness estimations to the heatmap ####
+#### Challenge: add CheckV completeness estimations to the heatmap (checkv doesn't work on bins)####
 On day 1, you ran [CheckV](https://www.nature.com/articles/s41587-020-00774-7) to estimate the completeness of viral contigs. Add this data as a column annotation to the heatmap. Consult the [ComplexHeatmap documentation](https://jokergoo.github.io/ComplexHeatmap-reference/book/) if necessary.
 
 --> we see that there are a few complete or high quality genomes, but most of the sequences appear to be incomplete.
--- code/day4/clustering.R
+++ code/day4/clustering.R
@@ -1,10 +1,9 @@
-# 0. Libraries & load data --------------------------------------------------
+# 0. Libraries  -----------------------------------------------------------
 library(tidyverse)
 library(ComplexHeatmap)
 
-
 # 1. Read data ------------------------------------------------------------
-pc_file <- "/home/jeroen/course-jena/annotated_clusters.txt" # Location of annotated protein clusters made on day 3 of the workshop
+pc_file <- "../../data/day_4/annotated_clusters.txt" # Location of annotated protein clusters made on day 3 of the workshop
 proteins <- data.table::fread(pc_file, header = TRUE)
 
 
@@ -12,7 +11,7 @@ proteins <- data.table::fread(pc_file, header = TRUE)
 # Take a moment to look what is in this table. What are the columns?
 View(proteins)
 
-# histogram of cluster sizes
+# Histogram of cluster sizes (optional)
 proteins %>% 
   count(cluster_id) %>% 
   ggplot() + 
@@ -21,40 +20,120 @@ proteins %>%
   ylab("Number of clusters")
 
 
-# Add some extra observables
+
+# 2.  Clean up data -------------------------------------------------------
+# add a few extra observables
 proteins <- proteins %>% 
   mutate(
+    # add unique id for contig
     seq_id = gsub("_[0-9]*$", "", member), 
+    
+    # is it RefSeq or one of our own contigs?
     seq_origin = case_when(str_detect(member, "NODE") ~ "assembly",
                        TRUE ~ "refseq"), 
+    
+    # get the length from the contig name, and add some high value for RefSeq sequences (too lazy to look up the RefSeq lengths :) 
     seq_length = case_when(seq_origin == "assembly" ~ as.numeric(gsub(".*length_|_cov.*", "", member)), 
-                       TRUE ~ 9999999 # some high number for refseq sequences -- could also try to get this info from RefSeq
+                       TRUE ~ 9999999 
                        )
   )
     
-# Drop some columns that we don't need
+# Drop the columns we don't need
 proteins <- select(proteins, reference, member, cluster_id, members, prodigal, Category, RefSeq, seq_id, seq_origin, seq_length)
 
+# Add binning information
+bins <- data.table::fread("../../data/day_4/bin_stats.txt")
+
+# we need to split the contig column
+# This is a bit messy, if you know a better way to do this please let me know :)
+nmax <- max(stringr::str_count(bins$contig, "\\,")) + 1 # max nr. of contigs in bin
+
+bins <- bins %>% 
+  separate(contigs,
+           sep = ",",
+           into = paste0("contig_", seq_len(nmax))) %>% 
+  pivot_longer(
+    cols = starts_with("contig"),
+    names_to = c(".value", "item"),
+    names_sep = "_"
+  ) %>%
+  drop_na("contig") %>%
+  select(-item)
+
+# @Dani: it seems not all contigs in bins are in the protein clustering table!
+table(bins$contig %in% proteins$seq_id)
+
+# add the bin annotation to the protein data frame
+proteins <- left_join(proteins, bins, by = c("seq_id" = "contig"))
 
 
-# 2. Cluster contigs ------------------------------------------------------
-# Make clustering of proteins WITHOUT RefSeq proteins
+# make the bin heatmap
+prot_wide <- 
+  proteins %>% 
+  filter(seq_origin == "assembly") %>% # only keep contigs from the assembly; removes RefSeq Sequences
+  filter(!is.na(bin_id)) %>% 
+  select(bin_id, cluster_id) %>% 
+  
+  # convert from 'long' to 'wide' format. Look at prot_wide and proteins if you don't know what that is. 
+  pivot_wider(names_from = cluster_id, 
+              values_from = cluster_id, 
+              values_fill = 0, 
+              values_fn = function(x) 1) 
+
+# Only keep protein presence/absence for plotting
+for_heatmap <- 
+  select(prot_wide, -bin_id) %>% 
+  as.matrix()
+
+# make heatmap object
+hmap <- Heatmap(for_heatmap, 
+                show_column_names = FALSE, 
+                col = c("white", "blue"), 
+                clustering_distance_columns = "euclidean",
+                clustering_distance_rows = "euclidean",
+                name = "PC presence", 
+                column_title = paste(ncol(for_heatmap), "Protein clusters (MMSeqs2)"), 
+                row_title = paste(nrow(for_heatmap), "bins"), 
+                column_dend_height = unit(4, "cm"), 
+                row_dend_width = unit(4, "cm"), 
+                use_raster = TRUE, 
+                border = TRUE
+)
+
+# Draw the heatmap object 
+draw(hmap)
+
+# If you want to save to a file, do it like this:
+pdf("heatmap.pdf", width = 12, height = 8)
+draw(hmap)
+dev.off() # don't forget to close the file!
+
+
+
+
+# Old code: cluster contigs instead of bins ------------------------------------------------------
+# Let's first do a clustering WITHOUT any RefSeq proteins/sequences
 length_cutoff = 5000
+
 prot_wide <- 
   proteins %>% 
   filter(seq_length > length_cutoff) %>% 
   filter(seq_origin == "assembly") %>% # only keep contigs from the assembly; removes RefSeq Sequences
   select(seq_id, seq_length, seq_origin, cluster_id) %>% 
+  
+  # convert from 'long' to 'wide' format. Look at prot_wide and proteins if you don't know what that is. 
   pivot_wider(names_from = cluster_id, 
               values_from = cluster_id, 
               values_fill = 0, 
               values_fn = function(x) 1)
 
+# Only keep protein presence/absence for plotting
 for_heatmap <- 
   select(prot_wide, -seq_id, -seq_length, -seq_origin) %>% 
   as.matrix()
 
-ht <- Heatmap(for_heatmap, 
+# make heatmap object
+hmap <- Heatmap(for_heatmap, 
         show_column_names = FALSE, 
         col = c("white", "blue"), 
         clustering_distance_columns = "euclidean",
@@ -67,49 +146,9 @@ ht <- Heatmap(for_heatmap,
         use_raster = TRUE, 
         border = TRUE
 )
- 
-draw(ht)
 
-# . . Interpret what you see. 
-# . . (1) it is a very sparse matrix, so the majority of viral sequences shares 
-# . . . . very little genes
-# . . (2) Some contigs cluster together based on shared protein clusters. 
+# Draw the heatmap object 
+draw(hmap)
 
 
-# 3. Add RefSeq to heatmap ------------------------------------------------
-set.seed(786)
-
-
-
-# Add binning information -------------------------------------------------
-bins <- data.table::fread("/home/jeroen/teaching/2022-jena-viromics/data/day_4/bin_stats.txt")
-
-
-# filter by finding closest RefSeq hits
-
-# A: RefSeq is sparse: lot of uncharted viral diversity
-# . . Zoom in on a cluster ------------------------------------------------
-
-# What proteins are shared? Do you think these are the same genus? Why or why not?
-# Answer: If there are several distinct RefSeq in the cluster that agree on that taxonomic 
-# level, and this is robust. 
-#
-# Q: how would you improve your belief in this taxonomic assignment?
-# A: read up on classification: perhaps there are hallmark genes. Look for conserved synteny.
-#
-# What would limit this clustering approahc? 
-# --> fragmented genomes
-# n. Add CheckV output -------------------------------------------------------
-#
-# Read XX of the CheckV paper. Discuss why viral taxonomy is an iterative process. 
-# add checkV annotation to heatmap
-
-
-# Conserved genes ---------------------------------------------------------
-# Look for terminase Large subunit (and portal and MCap?)
-# add those to the heatmap
-# make phylogeny based on these marker genes
-
-
-# Gene sharing networks
 
